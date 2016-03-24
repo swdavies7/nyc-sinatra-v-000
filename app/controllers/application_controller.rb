17a911eb48004b27bced8468b9dc780786e0b8de
swdavies7 <swdavies7@gmail.com>
2016-03-24 02:52:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/' do
+    erb :'/application/root'
+  end
+
 end
---
+++ db/migrate/05_alter_year_completed.rb
@@ -0,0 +1,6 @@
+class AlterYearCompleted < ActiveRecord::Migration
+
+  def change
+    change_column :landmarks, :year_completed, :integer
+  end
+end
